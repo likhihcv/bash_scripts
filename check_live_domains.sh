74313f6d47b8854e766f2f7fa128acea9dd03801
likhihcv <36541248+likhihcv@users.noreply.github.com>
2021-03-22 15:50:32 +0400

    Update check_live_domains.sh

--- check_live_domains.sh
+++ check_live_domains.sh
@@ -1,5 +1,5 @@
 #check if the domain/subdoamin exists or not. it works by trying to fetch the ip. if ip for domain/subdomain exits then the domain/subdomain is working. but service might me running on some other port
-
+#command: ./check_live_domains.sh [file_name has=ving domain]
 trap "exit" INT
 while read F  ; do
         host $F | grep "has address" | cut -d " " -f1
